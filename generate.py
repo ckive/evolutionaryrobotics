e980e7ca7a94db54fab3e8f75ec9ee812f4a7fa2
Dan Yang <56417240+ckive@users.noreply.github.com>
2023-01-10 14:47:58 -0600

    correct joints and links

--- generate.py
+++ generate.py
@@ -5,6 +5,7 @@ def CreateWorld():
     # pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
     pyrosim.End()
 
+# WRONG WAY
 def CreateRobot():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Link0", pos=[0,0,0.5] , size=[1,1,1])
@@ -12,11 +13,12 @@ def CreateRobot():
     pyrosim.Send_Cube(name="Link1", pos=[0.5,0,0.5] , size=[1,1,1])
     pyrosim.End()
 
+# CORRECT WAY
 def Create3L2J():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Link0", pos=[0.5,0,0.5] , size=[1,1,1])
+    pyrosim.Send_Cube(name="Link0", pos=[1.5,0,1.5] , size=[1,1,1])
     pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [1,0,1])
-    pyrosim.Send_Cube(name="Link1", pos=[0.5,0,0.5] , size=[1,1,1])
+    pyrosim.Send_Cube(name="Link1", pos=[-0.5,0,-0.5] , size=[1,1,1])
     pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [1,0,0])
     pyrosim.Send_Cube(name="Link2", pos=[0.5,0,-0.5] , size=[1,1,1])
     pyrosim.End()
